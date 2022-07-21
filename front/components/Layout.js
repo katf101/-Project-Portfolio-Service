3a90dba8ecb6f91abe3abfb1ddabd99fb9e2a83b
katf101 <vawav11@gmail.com>
2022-07-21 15:02:35 +0900

    07/21 해더 (홈, 로그인) 로그인 임시 css

--- front/components/Layout.js
+++ front/components/Layout.js
@@ -4,28 +4,79 @@ import styled from "styled-components";
 
 const Layout = ({ children }) => {
   return (
-    <Header>
-      <Link href="/">홈</Link>
-      <Link href="/post/detail">상세게시글</Link>
-      <Link href="/mypage/resume">이력서</Link>
-      <Link href="/mypage/profile">프로필</Link>
-      <Link href="/mypage/notification">알림</Link>
-      <Link href="/mypage/message">메세지</Link>
-      <Link href="/log/signin">로그인</Link>
-      <Link href="/log/signup">회원가입</Link>
+    <>
+      <Header>
+        <div>
+          <div>
+            <Link href="/">홈</Link>
+          </div>
+        </div>
+        <div>
+          <Link href="/log/signin">
+            <SigninButton>로그인</SigninButton>
+          </Link>
+        </div>
+        {/* <Link href="/">홈</Link> */}
+        {/* <Link href="/post/detail">상세게시글</Link> */}
+        {/* <Link href="/mypage/resume">이력서</Link> */}
+        {/* <Link href="/mypage/profile">프로필</Link> */}
+        {/* <Link href="/mypage/notification">알림</Link> */}
+        {/* <Link href="/mypage/message">메세지</Link> */}
+        {/* <Link href="/log/signin">로그인</Link> */}
+        {/* <Link href="/log/signup">회원가입</Link> */}
+      </Header>
       {children}
-    </Header>
+    </>
   );
 };
 
 export default Layout;
 
-const Header = styled.header`
+const SigninButton = styled.button`
+  margin-right: 15%;
+
+  width: 55px;
+  height: 30px;
+
+  background: #ffffff;
+  border: 1px solid #e0581d;
+  border-radius: 10px;
+
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 8px;
+  line-height: 15px;
+
+  &:hover {
+    border: 1px solid #00f9ea;
+  }
+`;
+
+const Header = styled.div`
   width: 100%;
   height: 37px;
 
+  display: flex;
+
   background: #ffffff;
   border-bottom: 1px solid #e3e3e3;
+  div {
+    &:nth-child(1) {
+      width: 50%;
+      height: 37px;
+
+      flex-direction: row;
+      /* background: #d9d9d9; */
+    }
+    &:nth-child(2) {
+      width: 50%;
+      height: 37px;
+      display: flex;
+      flex-direction: row-reverse;
+      /* background: #d9d9d9; */
+    }
+  }
 
   @media (min-width: 320px) and (max-width: 500px) {
     width: 500px;
--- front/pages/log/signin.js
+++ front/pages/log/signin.js
@@ -1,8 +1,72 @@
 import React from "react";
+import styled from "styled-components";
 import Layout from "../../components/Layout";
+import Link from "next/link";
 
 const signin = () => {
-  return <Layout>로그인</Layout>;
+  return (
+    <Layout>
+      <MainDiv>
+        <input type="text" />
+        <input type="text" />
+        <div>
+          <Link href="/log/signup">회원가입</Link>
+        </div>
+        <button>로그인</button>
+      </MainDiv>
+    </Layout>
+  );
 };
 
 export default signin;
+
+const MainDiv = styled.div`
+  margin-left: 10%;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 80%;
+  height: 960px;
+  left: 276px;
+  top: 86px;
+
+  /* background: #d9d9d9; */
+
+  input {
+    margin-top: 30px;
+
+    width: 331px;
+    height: 40px;
+
+    background: #ffffff;
+    border: 1px solid #e0581d;
+    border-radius: 15px;
+  }
+  div {
+    &:nth-child(3) {
+      margin-top: 15px;
+
+      width: 112px;
+      height: 33px;
+
+      text-align: center;
+
+      /* background: #964545; */
+    }
+  }
+  button {
+    /* Rectangle 23 */
+
+    width: 144px;
+    height: 34px;
+
+    background: #b9dfeb;
+    border-radius: 15px;
+    border: 0px;
+    &:hover {
+      background: #5ec7e9;
+    }
+  }
+`;
