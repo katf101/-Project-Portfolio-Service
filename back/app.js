85730252c2a0973349958911814e7386846eb1ad
katf101 <vawav11@gmail.com>
2022-07-22 16:14:59 +0900

    쿠키, 세션 구현

--- back/app.js
+++ back/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
 
 const userRouter = require("./routes/user");
 // const upostRouter = require("./routes/post");
 const db = require("./models");
 const passportConfig = require("./passport");
 
+dotenv.config();
 const app = express();
 
 db.sequelize
@@ -25,6 +30,16 @@ app.use(
 );
 app.use(express.json()); // 프론트에서 data를 json으로 보냈을때 req.body 안에 넣어줌
 app.use(express.urlencoded({ extended: true })); // 프론트에서 form(form 은 url)으로 data를 보냇을때 req.body 안에 넣어줌
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    saveUninitalized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", (req, res) => {
   res.send("hello express");
