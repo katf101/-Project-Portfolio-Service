5eadbb3bd8786bccb7cc1bd8e2e416607c3d2e9e
katf101 <vawav11@gmail.com>
2022-08-11 17:28:19 +0900

    back db port change

--- back/config/config.js
+++ back/config/config.js
@@ -8,7 +8,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: "pf-service",
     host: "127.0.0.1",
-    port: "3308",
+    port: "3306",
     dialect: "mysql",
   },
   test: {
