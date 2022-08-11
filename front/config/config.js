fefa6544e5189e65cdef5ecd0ef352b10ba49f04
katf101 <vawav11@gmail.com>
2022-08-11 20:32:04 +0900

    config url 설정

--- front/config/config.js
+++ front/config/config.js
@@ -1,6 +1,7 @@
 const prod = process.env.NODE_ENV === "production";
 module.exports = {
-  backendUrl: prod ? "https://fori.com" : "http://15.164.220.119/",
+  backendUrl: "http://15.164.220.119/",
+  // backendUrl: prod ? "http://foli.com" : "http://15.164.220.119/",
   // backendUrl: prod ? "https://api.fori.com" : "http://localhost:3060",
   // imageUrl: "http://localhost:3060/image",
   imageUrl: prod ? null : "http://15.164.220.119//images",
