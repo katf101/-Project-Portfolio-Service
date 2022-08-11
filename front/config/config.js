3156a6ae22e828e5514bf9de6e3d78c76d2d26da
katf101 <vawav11@gmail.com>
2022-08-11 18:40:56 +0900

    3000 -> 80

--- front/config/config.js
+++ front/config/config.js
@@ -1,7 +1,8 @@
 const prod = process.env.NODE_ENV === "production";
 module.exports = {
-  backendUrl: prod ? "https://api.fori.com" : "http://15.164.220.119/",
+  backendUrl: prod ? "https://fori.com" : "http://15.164.220.119/",
   // backendUrl: prod ? "https://api.fori.com" : "http://localhost:3060",
   // imageUrl: "http://localhost:3060/image",
-  imageUrl: prod ? null : "http://localhost:3060/images",
+  imageUrl: prod ? null : "http://15.164.220.119//images",
+  // imageUrl: prod ? null : "http://localhost:3060/images",
 };
