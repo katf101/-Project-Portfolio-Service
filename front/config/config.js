fc9128ad132d9065250b8d095b3e4efac6aea387
katf101 <vawav11@gmail.com>
2022-08-11 18:38:52 +0900

    fromt 백엔드url aws 로 수정

--- front/config/config.js
+++ front/config/config.js
@@ -1,6 +1,7 @@
 const prod = process.env.NODE_ENV === "production";
 module.exports = {
-  backendUrl: prod ? "https://api.fori.com" : "http://localhost:3060",
+  backendUrl: prod ? "https://api.fori.com" : "http://15.164.220.119/",
+  // backendUrl: prod ? "https://api.fori.com" : "http://localhost:3060",
   // imageUrl: "http://localhost:3060/image",
   imageUrl: prod ? null : "http://localhost:3060/images",
 };
