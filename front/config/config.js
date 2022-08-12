ec0eccc21508e9fa018a2a844a85ff0394e02277
katf101 <vawav11@gmail.com>
2022-08-12 18:58:03 +0900

    front config.js 수정

--- front/config/config.js
+++ front/config/config.js
@@ -1,7 +1,9 @@
 const prod = process.env.NODE_ENV === "production";
 module.exports = {
   // backendUrl: "http://api.semifoli.site/",
-  backendUrl: prod ? "https://api.semifoli.site" : "http://localhost:3060",
+  // backendUrl: prod ? "http://foli.com" : "http://15.164.220.119/",
+  backendUrl: prod ? "http://api.fori.com" : "http://localhost:3060",
   // imageUrl: "http://localhost:3060/image",
   // imageUrl: prod ? null : "http://api.semifoli.site/images",
+  // imageUrl: prod ? null : "http://localhost:3060/images",
 };
