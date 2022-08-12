4c3a11e8c219fab75b4b091171a554132f534c1c
katf101 <vawav11@gmail.com>
2022-08-12 19:10:57 +0900

    fori -> foli

--- front/config/config.js
+++ front/config/config.js
@@ -2,7 +2,7 @@ const prod = process.env.NODE_ENV === "production";
 module.exports = {
   // backendUrl: "http://api.semifoli.site/",
   // backendUrl: prod ? "http://foli.com" : "http://15.164.220.119/",
-  backendUrl: prod ? "http://api.semifori.site/" : "http://localhost:3060/",
+  backendUrl: prod ? "http://api.semifoli.site/" : "http://localhost:3060/",
   // imageUrl: "http://localhost:3060/image",
   // imageUrl: prod ? null : "http://api.semifoli.site/images",
   // imageUrl: prod ? null : "http://localhost:3060/images",
