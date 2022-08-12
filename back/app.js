5c3213da4d7e2487415caa9c5427dbfc4278952e
katf101 <vawav11@gmail.com>
2022-08-12 19:16:48 +0900

    도메인 고정

--- back/app.js
+++ back/app.js
@@ -64,7 +64,8 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: false,
-      domain: process.env.NODE_ENV === "production" && ".semifoli.site",
+      domain: ".semifoli.site",
+      // domain: process.env.NODE_ENV === "production" && ".semifoli.site",
     },
   })
 );
