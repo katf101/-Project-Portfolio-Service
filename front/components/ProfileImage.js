276e6471a21bbf7c2c2727d2cbc8c3038a211841
katf101 <vawav11@gmail.com>
2022-08-14 00:56:14 +0900

    test

--- front/components/ProfileImage.js
+++ front/components/ProfileImage.js
@@ -139,7 +139,7 @@ const ProfileImage = ({ imagedata, setImageData }) => {
             {imagePaths.map((v, i) => (
               <div key={v} style={{ display: "inline-block" }}>
                 <PreviewImg
-                  src={`/${v}`}
+                  src={`${v}`}
                   width={150}
                   height={150}
                   // src={`${backendUrl}/${v}`} // dev
